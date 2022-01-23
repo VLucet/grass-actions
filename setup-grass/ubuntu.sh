829e63d7a61f0bafac187b5fa2159888f90886e0
Valentin <valentin.lucet@gmail.com>
2022-01-23 13:17:19 -0500

    updates

---
+++ setup-grass/ubuntu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Get dependencites
+sudo apt-get update -y
+sudo apt-get install -y wget git gawk findutils xargs -a <(awk '! /^ *(#|$)/' ".github/workflows/apt.txt") -r -- \
+sudo apt-get install -y --no-install-recommends --no-install-suggests
+
+# Create Install directory
+mkdir $HOME/install
+
+# Set number of cores for compilation
+echo "MAKEFLAGS=-j$(nproc)" >> $GITHUB_ENV
+
+# Set LD_LIBRARY_PATH for compilation
+echo "LD_LIBRARY_PATH=$HOME/install/lib" >> $GITHUB_ENV
+
+# Build GRASS GIS
+
+
+# Add th binary directory to PATH
+echo "$HOME/install/bin" >> $GITHUB_PATH
