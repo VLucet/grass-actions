fb2847d1a9cd48d5cf9b7da55aec28f492ee720d
Valentin <valentin.lucet@gmail.com>
2022-01-23 15:30:35 -0500

    TRY FIX

--- setup-grass/ubuntu.sh
+++ setup-grass/ubuntu.sh
@@ -9,12 +9,6 @@ sudo apt-get install -y wget git gawk findutils
 xargs -a <(awk '! /^ *(#|$)/' ".github/workflows/apt.txt") -r -- \
     sudo apt-get install -y --no-install-recommends --no-install-suggests
 
-# Install dependencies
-# sudo apt install -y libzstd-dev
-# sudo apt install -y proj
-# sudo apt install -y libgdal-dev
-# sudo apt install -y freeglut3-dev
-
 # Create Install directory
 mkdir $HOME/install
 
@@ -26,12 +20,6 @@ echo "LD_LIBRARY_PATH=$HOME/install/lib" >> $GITHUB_ENV
 
 # Build GRASS GIS
 .github/workflows/build.sh $HOME/install
-
-# Add the binary directory to PATH
-echo "$HOME/install/bin" >> $GITHUB_PATH
-
-# Tests
-.github/workflows/test_simple.sh
 
 # Move back home
 cd $HOME
