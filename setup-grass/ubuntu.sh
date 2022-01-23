423942a7b5898e7ebbf7d85fdc4422b02f88da04
Valentin <valentin.lucet@gmail.com>
2022-01-23 14:55:54 -0500

    make sure deps are installed

--- setup-grass/ubuntu.sh
+++ setup-grass/ubuntu.sh
@@ -2,14 +2,14 @@
 
 # Get dependencites
 sudo apt-get update -y
-sudo apt-get install -y wget git gawk findutils xargs -a <(awk '! /^ *(#|$)/' "grass/.github/workflows/apt.txt") -r -- \
+sudo apt-get install -y wget git gawk findutils xargs -a <(awk '! /^ *(#|$)/' ".github/workflows/apt.txt") -r -- \
 sudo apt-get install -y --no-install-recommends --no-install-suggests
 
 # Install dependencies
-sudo apt install -y libzstd-dev
-sudo apt install -y proj
-sudo apt install -y libgdal-dev
-sudo apt install -y freeglut3-dev
+# sudo apt install -y libzstd-dev
+# sudo apt install -y proj
+# sudo apt install -y libgdal-dev
+# sudo apt install -y freeglut3-dev
 
 # Create Install directory
 mkdir $HOME/install
