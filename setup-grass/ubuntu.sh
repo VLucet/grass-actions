0f2a4f96a04e4909f6c603780a9d4c9eaef212c5
Valentin <valentin.lucet@gmail.com>
2022-03-27 15:42:02 -0400

    try with 8

--- setup-grass/ubuntu.sh
+++ setup-grass/ubuntu.sh
@@ -9,15 +9,6 @@ sudo apt-get install -y wget git gawk findutils
 xargs -a <(awk '! /^ *(#|$)/' ".github/workflows/apt.txt") -r -- \
     sudo apt-get install -y --no-install-recommends --no-install-suggests
 
-# # Create Install directory
-# mkdir $HOME/install
-
-# # Set number of cores for compilation
-# echo "MAKEFLAGS=-j$(nproc)" >> $GITHUB_ENV
-
-# # Set LD_LIBRARY_PATH for compilation
-# echo "LD_LIBRARY_PATH=$HOME/install/lib" >> $GITHUB_ENV
-
 # Build GRASS GIS
 .github/workflows/build.sh $HOME/install
 
